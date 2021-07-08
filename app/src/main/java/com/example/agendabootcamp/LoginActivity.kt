f4191a676d947c464c5f73684ff6017c4e1b0e65
Adiel Ferreira <adiel.ferreira26@gmail.com>
2021-07-08 16:49:40 -0300

    Adicionado sistema de auth login do Firebase

--- app/src/main/java/com/example/agendabootcamp/LoginActivity.kt
+++ app/src/main/java/com/example/agendabootcamp/LoginActivity.kt
@@ -4,22 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        auth = FirebaseAuth.getInstance()
         button_entrar.setOnClickListener{
-            login()
+            login(edit_text_email.text.toString(),edit_text_password.text.toString())
         }
     }
 
-    private fun login(){
-        if(edit_text_email.text.toString() == "adiel" && edit_text_password.text.toString() == "cidoso"){
-            Toast.makeText(this,"Login Bem Sucedido!",Toast.LENGTH_LONG).show()
-            this.finish()
+    private fun login(email:String, senha:String){
+
+        auth?.signInWithEmailAndPassword(email,senha)?.addOnCompleteListener(this){ task->
+
+            if(task.isSuccessful){
+                Toast.makeText(this,"Login Bem Sucedido!",Toast.LENGTH_LONG).show()
+                this.finish()
+            }else{
+                Toast.makeText(this,"Login ou senha incorreta!",Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
